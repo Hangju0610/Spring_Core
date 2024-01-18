efe4ced0720423ac9bf074c8a04dae881505381d
hangju0610 <phj950610@naver.com>
2024-01-18 11:08:06 +0900

    Feat: Add AutoAppConfig

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
     // 현재 다른 저장소로 변경할 때, OCP 원칙을 준수하지 않고 있음
     // MemoryMemberRepository 클래스에 의존 및 memberRepository 인터페이스에 의존
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
