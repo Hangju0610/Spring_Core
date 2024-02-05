7d233dbbde2b54cb7e36c670713a20d2c988bc09
Park HJ <phj950610@epopcon.com>
2024-02-06 00:26:01 +0900

    Refac: Add bean Config

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url;
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
@@ -19,18 +16,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("call: " + url + " message = " + message);
     }
     //서비스 종료시 호출
-    public void disconnect() {
+    public void disConnect() {
         System.out.println("close + " + url);
     }
 
-    @Override
-    public void destroy() throws Exception {
-        disconnect();
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
+    }
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disConnect();
     }
 }
